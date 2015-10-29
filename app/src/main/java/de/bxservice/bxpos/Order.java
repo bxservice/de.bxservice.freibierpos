2365b7e13625e31bb9f1edc149f4334e3af05392
Diego Ruiz <12065321+d-ruiz@users.noreply.github.com>
2015-10-29 15:56:53 +0100

    Edit order list view complete

---
+++ app/src/main/java/de/bxservice/bxpos/Order.java
@@ -0,0 +1,34 @@
+package de.bxservice.bxpos;
+
+/**
+ * Created by diego on 29/10/15.
+ */
+public class Order {
+
+    private String orderName;
+    private String value;
+
+    public String getOrderName() {
+        return orderName;
+    }
+
+    public void setOrderName(String orderName) {
+        this.orderName = orderName;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public Order(String order, String v){
+        orderName= order;
+        value = v;
+    }
+
+    @Override
+    public String toString(){return orderName+","+value;}
+}
