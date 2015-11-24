0e61a1b9779352661d6adfce1a54733387816cfe
tbayen_bxservice <tbayen_bxservice@localhost>
2015-11-24 13:10:55 +0100

    Search shows a ListView

--- app/src/main/java/de/bxservice/bxpos/ui/EditOrderActivity.java
+++ app/src/main/java/de/bxservice/bxpos/ui/EditOrderActivity.java
@@ -22,6 +22,7 @@ import com.astuetz.PagerSlidingTabStrip;
 
 import de.bxservice.bxpos.persistence.OrderDataExample;
 import de.bxservice.bxpos.R;
+import de.bxservice.bxpos.ui.adapter.OrderArrayAdapter;
 
 public class EditOrderActivity extends AppCompatActivity {
 
--- app/src/main/java/de/bxservice/bxpos/ui/SearchMenuItemActivity.java
+++ app/src/main/java/de/bxservice/bxpos/ui/SearchMenuItemActivity.java
@@ -4,14 +4,23 @@ import android.app.SearchManager;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 
+import java.text.NumberFormat;
+import java.util.ArrayList;
 import java.util.List;
 
 import de.bxservice.bxpos.R;
 import de.bxservice.bxpos.logic.DataMediator;
+import de.bxservice.bxpos.logic.model.Order;
 import de.bxservice.bxpos.logic.model.Product;
+import de.bxservice.bxpos.logic.model.ProductPrice;
+import de.bxservice.bxpos.ui.adapter.OrderArrayAdapter;
 
 public class SearchMenuItemActivity extends AppCompatActivity {
+
+    private ListView listView;
+    private OrderArrayAdapter<String> mAdapter;
 
 
 
@@ -20,6 +29,7 @@ public class SearchMenuItemActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_menu_item);
 
+        listView = (ListView) findViewById(R.id.list_search_View);
         handleIntent(getIntent());
     }
 
@@ -33,17 +43,27 @@ public class SearchMenuItemActivity extends AppCompatActivity {
 
         if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
             String query = intent.getStringExtra(SearchManager.QUERY);
+            List items = new ArrayList<Order>();
 
             List<Product> p =  DataMediator.getInstance().getProductList();
 
+            NumberFormat currencyFormat = NumberFormat.getCurrencyInstance(DataMediator.LOCALE);
+            ProductPrice productPrice;
+
             for (Product pa : p){
                 boolean b = pa.getProductName().matches("(?i).*" + query + ".*");
-                if(b)
-                    System.out.println(pa.getProductName());
-
+                if(b){
+                    productPrice = DataMediator.getInstance().getProductPriceHashMap().get(pa);
+                    items.add(new Order(pa.getProductName(), currencyFormat.format(productPrice.getStdPrice())));
+                }
             }
 
+            mAdapter = new OrderArrayAdapter<>(this, items);
 
+
+
+
+            listView.setAdapter(mAdapter);
 
             //use the query to search your data somehow
         }
