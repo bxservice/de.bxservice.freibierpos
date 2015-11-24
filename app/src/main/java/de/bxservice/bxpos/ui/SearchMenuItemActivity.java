e93dd3bbee21e622325746f8711bad67e7c208c9
Diego Ruiz <12065321+d-ruiz@users.noreply.github.com>
2015-11-24 14:59:10 +0100

    Improve search option for meny items

--- app/src/main/java/de/bxservice/bxpos/ui/SearchMenuItemActivity.java
+++ app/src/main/java/de/bxservice/bxpos/ui/SearchMenuItemActivity.java
@@ -1,10 +1,11 @@
 package de.bxservice.bxpos.ui;
 
-import android.app.SearchManager;
-import android.content.Intent;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.widget.ListView;
+import android.widget.SearchView;
 
 import java.text.NumberFormat;
 import java.util.ArrayList;
@@ -17,10 +18,12 @@ import de.bxservice.bxpos.logic.model.Product;
 import de.bxservice.bxpos.logic.model.ProductPrice;
 import de.bxservice.bxpos.ui.adapter.OrderArrayAdapter;
 
-public class SearchMenuItemActivity extends AppCompatActivity {
+public class SearchMenuItemActivity extends AppCompatActivity implements SearchView.OnQueryTextListener{
 
+    private SearchView mSearchView;
     private ListView listView;
     private OrderArrayAdapter<String> mAdapter;
+    private List items = new ArrayList<Order>();
 
 
 
@@ -29,43 +32,48 @@ public class SearchMenuItemActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_menu_item);
 
+        mSearchView = (SearchView) findViewById(R.id.search_items_view);
         listView = (ListView) findViewById(R.id.list_search_View);
-        handleIntent(getIntent());
+
+        initItems();
+
+        mAdapter = new OrderArrayAdapter<>(this, items);
+
+        listView.setAdapter(mAdapter);
+
+        setupSearchView();
     }
 
-    @Override
-    protected void onNewIntent(Intent intent) {
+    public void initItems(){
 
-        handleIntent(intent);
-    }
+        ProductPrice productPrice;
+        NumberFormat currencyFormat = NumberFormat.getCurrencyInstance(DataMediator.LOCALE);
 
-    private void handleIntent(Intent intent) {
-
-        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
-            String query = intent.getStringExtra(SearchManager.QUERY);
-            List items = new ArrayList<Order>();
-
-            List<Product> p =  DataMediator.getInstance().getProductList();
-
-            NumberFormat currencyFormat = NumberFormat.getCurrencyInstance(DataMediator.LOCALE);
-            ProductPrice productPrice;
-
-            for (Product pa : p){
-                boolean b = pa.getProductName().matches("(?i).*" + query + ".*");
-                if(b){
-                    productPrice = DataMediator.getInstance().getProductPriceHashMap().get(pa);
-                    items.add(new Order(pa.getProductName(), currencyFormat.format(productPrice.getStdPrice())));
-                }
-            }
-
-            mAdapter = new OrderArrayAdapter<>(this, items);
-
-
-
-
-            listView.setAdapter(mAdapter);
-
-            //use the query to search your data somehow
+        for( Product product : DataMediator.getInstance().getProductList() ){
+            productPrice = DataMediator.getInstance().getProductPriceHashMap().get(product);
+            items.add(new Order(product.getProductName(), currencyFormat.format(productPrice.getStdPrice())));
         }
+    }
+
+    public boolean onQueryTextChange(String newText) {
+        OrderArrayAdapter adapter = (OrderArrayAdapter)listView.getAdapter();
+
+        if ( TextUtils.isEmpty(newText) ) {
+            adapter.getFilter().filter(null);
+        } else {
+            adapter.getFilter().filter(newText);
+        }
+        return true;
+    }
+
+    public boolean onQueryTextSubmit(String query) {
+        return false;
+    }
+
+    private void setupSearchView() {
+        mSearchView.setIconifiedByDefault(false);
+        mSearchView.setOnQueryTextListener(this);
+        mSearchView.setSubmitButtonEnabled(false);
+        mSearchView.setQueryHint(getString(R.string.search));
     }
 }
