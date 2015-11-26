c03dbd756990722fb7fc55354528b9cd3874696b
tbayen_bxservice <tbayen_bxservice@localhost>
2015-11-26 15:01:41 +0100

    Improved search food items performance by not calling a new activity

--- app/src/main/java/de/bxservice/bxpos/ui/CreateOrderActivity.java
+++ app/src/main/java/de/bxservice/bxpos/ui/CreateOrderActivity.java
@@ -1,34 +1,48 @@
 package de.bxservice.bxpos.ui;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.app.SearchManager;
+import android.content.Context;
 import android.content.DialogInterface;
-import android.content.Intent;
+import android.os.Build;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
 
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.os.Bundle;
-import android.util.Log;
+import android.text.TextUtils;
 import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import com.astuetz.PagerSlidingTabStrip;
 
+import java.text.NumberFormat;
+import java.util.ArrayList;
+import java.util.List;
+
 import de.bxservice.bxpos.R;
+import de.bxservice.bxpos.logic.DataMediator;
 import de.bxservice.bxpos.logic.model.DraftOrder;
 import de.bxservice.bxpos.logic.model.MProduct;
+import de.bxservice.bxpos.logic.model.Order;
+import de.bxservice.bxpos.logic.model.ProductPrice;
 import de.bxservice.bxpos.ui.adapter.CreateOrderPagerAdapter;
+import de.bxservice.bxpos.ui.adapter.OrderArrayAdapter;
 import de.bxservice.bxpos.ui.dialog.GuestNumberDialogFragment;
 import de.bxservice.bxpos.ui.dialog.RemarkDialogFragment;
 
 public class CreateOrderActivity extends AppCompatActivity implements GuestNumberDialogFragment.GuestNumberDialogListener,
-        RemarkDialogFragment.RemarkDialogListener {
+        RemarkDialogFragment.RemarkDialogListener, SearchView.OnQueryTextListener {
 
     /**
      * The {@link android.support.v4.view.PagerAdapter} that will provide
@@ -39,6 +53,15 @@ public class CreateOrderActivity extends AppCompatActivity implements GuestNumbe
      * {@link android.support.v4.app.FragmentStatePagerAdapter}.
      */
     private CreateOrderPagerAdapter mCreateOrderPagerAdapter;
+
+    private PagerSlidingTabStrip tabs;
+    private View mTabFormView;
+
+    //ListView attributes for search functionality
+    private ListView listView;
+    private OrderArrayAdapter<String> mAdapter;
+    private List items = new ArrayList<Order>();
+    private SearchView mSearchView;
 
     //order attributes
     private DraftOrder draftOrder = null;
@@ -56,7 +79,7 @@ public class CreateOrderActivity extends AppCompatActivity implements GuestNumbe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_order_activity);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.create_order_toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
@@ -71,7 +94,18 @@ public class CreateOrderActivity extends AppCompatActivity implements GuestNumbe
         mViewPager = (ViewPager) findViewById(R.id.createOrderContainer);
         mViewPager.setAdapter(mCreateOrderPagerAdapter);
 
-        PagerSlidingTabStrip tabs = (PagerSlidingTabStrip) findViewById(R.id.tabs);
+
+        listView = (ListView) findViewById(R.id.list_search_View2);
+
+        initSearchListItems();
+
+        mAdapter = new OrderArrayAdapter<>(this, items);
+
+        listView.setAdapter(mAdapter);
+        listView.setVisibility(View.GONE);
+
+
+        tabs = (PagerSlidingTabStrip) findViewById(R.id.tabs);
         tabs.setViewPager(mViewPager);
 
         Toast.makeText(getBaseContext(), Integer.toString(getNumberOfGuests()) + " " + getSelectedTable(),
@@ -87,8 +121,98 @@ public class CreateOrderActivity extends AppCompatActivity implements GuestNumbe
             }
         });
 
+        mTabFormView = findViewById(R.id.createOrderContainer);
+
     }
 
+    /**
+     * Creates the list view that will be used when click on search
+     */
+    public void initSearchListItems() {
+
+        ProductPrice productPrice;
+        NumberFormat currencyFormat = NumberFormat.getCurrencyInstance(DataMediator.LOCALE);
+
+        for( MProduct product : DataMediator.getInstance().getProductList() ){
+            productPrice = DataMediator.getInstance().getProductPriceHashMap().get(product);
+            items.add(new Order(product.getProductName(), currencyFormat.format(productPrice.getStdPrice())));
+        }
+    }
+
+    /**
+     * Method that allows to capture the text that is being
+     * introduced in the search field
+     * @param newText
+     * @return
+     */
+    public boolean onQueryTextChange(String newText) {
+        OrderArrayAdapter adapter = (OrderArrayAdapter)listView.getAdapter();
+
+        if ( TextUtils.isEmpty(newText) ) {
+            adapter.getFilter().filter(null);
+        } else {
+            showSearchList(true);
+            adapter.getFilter().filter(newText);
+        }
+        return true;
+    }
+
+    public boolean onQueryTextSubmit(String query) {
+        return false;
+    }
+
+    /**
+     * When the search button is pressed - the listview is shown and the tabs are hidden
+     * When the search button loses focuses - listview hidden and tabs are shown
+     * @param show
+     */
+    private void showSearchList(final boolean show) {
+        // On Honeycomb MR2 we have the ViewPropertyAnimator APIs, which allow
+        // for very easy animations. If available, use these APIs to fade-in
+        // the progress spinner.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
+            int shortAnimTime = getResources().getInteger(android.R.integer.config_shortAnimTime);
+
+            mTabFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+            mTabFormView.animate().setDuration(shortAnimTime).alpha(
+                    show ? 0 : 1).setListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    mTabFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+                }
+            });
+
+            tabs.setVisibility(show ? View.GONE : View.VISIBLE);
+            tabs.animate().setDuration(shortAnimTime).alpha(
+                    show ? 0 : 1).setListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    tabs.setVisibility(show ? View.GONE : View.VISIBLE);
+                }
+            });
+
+            listView.setVisibility(show ? View.VISIBLE : View.GONE);
+            listView.animate().setDuration(shortAnimTime).alpha(
+                    show ? 1 : 0).setListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    listView.setVisibility(show ? View.VISIBLE : View.GONE);
+                }
+            });
+        } else {
+            // The ViewPropertyAnimator APIs are not available, so simply show
+            // and hide the relevant UI components.
+            listView.setVisibility(show ? View.VISIBLE : View.GONE);
+            mTabFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+            tabs.setVisibility(show ? View.GONE : View.VISIBLE);
+        }
+    }//showSearchList
+
+    /**
+     * Get extras from the previous activity
+     * - Table number
+     * - Number of guests
+     */
     public void getExtras() {
         Bundle extras = getIntent().getExtras();
 
@@ -107,7 +231,32 @@ public class CreateOrderActivity extends AppCompatActivity implements GuestNumbe
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_create_order_activity, menu);
 
+        SearchManager searchManager =
+                (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+
+        mSearchView = (SearchView) menu.findItem(R.id.items_search).getActionView();
+
+        mSearchView.setSearchableInfo(
+                searchManager.getSearchableInfo(getComponentName()));
+
+        setupSearchView();
+
         return true;
+    }
+
+    /**
+     * Add the needed congiguration to the search view
+     */
+    private void setupSearchView() {
+        mSearchView.setOnQueryTextListener(this);
+        mSearchView.setSubmitButtonEnabled(false);
+        mSearchView.setOnSearchClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                showSearchList(true);
+            }
+        });
     }
 
     @Override
@@ -119,16 +268,17 @@ public class CreateOrderActivity extends AppCompatActivity implements GuestNumbe
 
         //noinspection SimplifiableIfStatement
         if(id == android.R.id.home) {
+            //If the searchview mode is displayed, only go back to the tab
+            if ( listView.isShown() ) {
+                mSearchView.onActionViewCollapsed();
+                showSearchList(false);
+                return true;
+            }
             onBackPressed();
             return true;
         }
         if (id == R.id.set_guests) {
             showGuestNumberDialog();
-            return true;
-        }
-        if(id == R.id.items_search){
-            Intent intent = new Intent(this, SearchMenuItemActivity.class);
-            startActivity(intent);
             return true;
         }
         if (id == R.id.add_note) {
