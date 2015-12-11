9cd6df077bd253bcd26155eaa551695789f895ca
Diego Ruiz <12065321+d-ruiz@users.noreply.github.com>
2015-12-11 19:40:26 +0100

    Add better listener to ordering items

---
+++ app/src/main/java/de/bxservice/bxpos/ui/RecyclerOrderingItemsListener.java
@@ -0,0 +1,51 @@
+package de.bxservice.bxpos.ui;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.View;
+
+/**
+ * Listener for the lis items when ordering
+ * Created by Diego Ruiz on 11/12/15.
+ */
+public class RecyclerOrderingItemsListener implements RecyclerView.OnItemTouchListener {
+
+    private OnItemClickListener mListener;
+
+    public interface OnItemClickListener {
+        public void onItemClick(View view, int position);
+    }
+
+    GestureDetector mGestureDetector;
+
+    public RecyclerOrderingItemsListener(Context context, OnItemClickListener listener) {
+        mListener = listener;
+        mGestureDetector = new GestureDetector(context, new GestureDetector.SimpleOnGestureListener() {
+            @Override
+            public boolean onSingleTapUp(MotionEvent e) {
+                return true;
+            }
+        });
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(RecyclerView view, MotionEvent e) {
+        View childView = view.findChildViewUnder(e.getX(), e.getY());
+        if (childView != null && mListener != null && mGestureDetector.onTouchEvent(e)) {
+            mListener.onItemClick(childView, view.getChildAdapterPosition(childView));
+        }
+        return false;
+    }
+
+    @Override
+    public void onTouchEvent(RecyclerView view, MotionEvent motionEvent) {
+    }
+
+    @Override
+    public void onRequestDisallowInterceptTouchEvent(boolean disallowIntercept) {
+
+    }
+
+}
