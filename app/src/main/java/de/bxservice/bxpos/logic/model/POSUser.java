831887307c1db2745e85b3e07660c54f831f81b3
tbayen_bxservice <tbayen_bxservice@localhost>
2015-12-15 18:05:02 +0100

    Add sqllite helpers and user table support

---
+++ app/src/main/java/de/bxservice/bxpos/logic/model/POSUser.java
@@ -0,0 +1,35 @@
+package de.bxservice.bxpos.logic.model;
+
+/**
+ * Created by Diego Ruiz on 15/12/15.
+ */
+public class POSUser extends UserContract.User {
+
+    int id;
+    String username;
+    String password;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
