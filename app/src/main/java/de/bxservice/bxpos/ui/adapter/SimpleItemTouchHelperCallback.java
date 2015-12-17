492d604fa40d175b87d5c13bef5c78b30a2cae09
Diego Ruiz <12065321+d-ruiz@users.noreply.github.com>
2015-12-17 14:26:07 +0100

    Improve UI

--- app/src/main/java/de/bxservice/bxpos/ui/adapter/SimpleItemTouchHelperCallback.java
+++ app/src/main/java/de/bxservice/bxpos/ui/adapter/SimpleItemTouchHelperCallback.java
@@ -43,4 +43,30 @@ public class SimpleItemTouchHelperCallback extends ItemTouchHelper.Callback {
         mAdapter.onItemDismiss(viewHolder.getAdapterPosition());
     }
 
+    @Override
+    public void onSelectedChanged(RecyclerView.ViewHolder viewHolder,
+                                  int actionState) {
+        // We only want the active item
+        if (actionState != ItemTouchHelper.ACTION_STATE_IDLE) {
+            if (viewHolder instanceof ItemTouchHelperViewHolder) {
+                ItemTouchHelperViewHolder itemViewHolder =
+                        (ItemTouchHelperViewHolder) viewHolder;
+                itemViewHolder.onItemSelected();
+            }
+        }
+
+        super.onSelectedChanged(viewHolder, actionState);
+    }
+    @Override
+    public void clearView(RecyclerView recyclerView,
+                          RecyclerView.ViewHolder viewHolder) {
+        super.clearView(recyclerView, viewHolder);
+
+        if (viewHolder instanceof ItemTouchHelperViewHolder) {
+            ItemTouchHelperViewHolder itemViewHolder =
+                    (ItemTouchHelperViewHolder) viewHolder;
+            itemViewHolder.onItemClear();
+        }
+    }
+
 }
