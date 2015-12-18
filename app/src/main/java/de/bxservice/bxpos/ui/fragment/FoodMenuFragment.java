503dee5c0872abea945385f2fc83c482799e01a5
Diego Ruiz <12065321+d-ruiz@users.noreply.github.com>
2015-12-18 12:52:30 +0100

    Decouple FoodMenuFragment

---
+++ app/src/main/java/de/bxservice/bxpos/ui/fragment/FoodMenuFragment.java
@@ -0,0 +1,133 @@
+package de.bxservice.bxpos.ui.fragment;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.TextView;
+
+import java.text.NumberFormat;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import de.bxservice.bxpos.R;
+import de.bxservice.bxpos.logic.DataMediator;
+import de.bxservice.bxpos.logic.model.MProduct;
+import de.bxservice.bxpos.logic.model.NewOrderGridItem;
+import de.bxservice.bxpos.logic.model.ProductCategory;
+import de.bxservice.bxpos.logic.model.ProductPrice;
+import de.bxservice.bxpos.ui.CreateOrderActivity;
+import de.bxservice.bxpos.ui.adapter.GridOrderViewAdapter;
+
+/**
+ * A placeholder fragment containing a simple view.
+ * Created by Diego Ruiz on 18/12/15.
+ */
+public class FoodMenuFragment extends Fragment {
+    /**
+     * The fragment argument representing the section number for this
+     * fragment.
+     */
+    private static final String ARG_SECTION_NUMBER = "section_number";
+    private GridView grid;
+    private ArrayList<NewOrderGridItem> mGridData;
+    private GridOrderViewAdapter mGridAdapter;
+    private List<ProductCategory> productCategoryList;
+    private HashMap<NewOrderGridItem, MProduct> itemProductHashMap;
+
+    public ArrayList<NewOrderGridItem> getmGridData() {
+        return mGridData;
+    }
+
+    /**
+     * Returns a new instance of this fragment for the given section
+     * number.
+     */
+    public static FoodMenuFragment newInstance(int sectionNumber) {
+        FoodMenuFragment fragment = new FoodMenuFragment();
+        Bundle args = new Bundle();
+        args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public FoodMenuFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.fragment_create_order_activity, container, false);
+
+        int sectionNumber = getArguments().getInt(ARG_SECTION_NUMBER);
+
+        grid = (GridView) rootView.findViewById(R.id.create_order_gridview);
+
+        productCategoryList = DataMediator.getInstance().getProductCategoryList();
+
+        ProductCategory pc = productCategoryList.get(sectionNumber);
+
+        mGridData = new ArrayList<>();
+        itemProductHashMap = new HashMap<NewOrderGridItem, MProduct>();
+
+        NumberFormat currencyFormat = NumberFormat.getCurrencyInstance(DataMediator.LOCALE);
+
+        NewOrderGridItem item;
+        ProductPrice productPrice;
+        int qtyOrdered;
+        for(MProduct product : pc.getProducts()) {
+            item = new NewOrderGridItem();
+            item.setName(product.getProductName());
+
+            productPrice = DataMediator.getInstance().getProductPriceHashMap().get(product.getProductID());
+            item.setPrice(currencyFormat.format(productPrice.getStdPrice()));
+
+            //When you navigate through the tabs it paints again everything - this lets the number stay
+            qtyOrdered = ((CreateOrderActivity) getActivity()).getProductQtyOrdered(product);
+            if(qtyOrdered != 0)
+                item.setQty("x"+Integer.toString(qtyOrdered));
+            else
+                item.setQty("");
+
+            mGridData.add(item);
+            itemProductHashMap.put(item,product);
+        }
+
+        grid.setGravity(Gravity.CENTER_HORIZONTAL);
+        mGridAdapter = new GridOrderViewAdapter(this.getContext(), R.layout.food_menu_grid_item_layout, mGridData);
+
+        grid.setAdapter(mGridAdapter);
+
+        grid.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+
+                //Get item at position
+                NewOrderGridItem item = (NewOrderGridItem) parent.getItemAtPosition(position);
+                TextView qtyOrderedTextView = (TextView) v.findViewById(R.id.qtyOrdered_text);
+
+                MProduct product = itemProductHashMap.get(item);
+
+                ((CreateOrderActivity) getActivity()).addOrderItem(product);
+
+                int productQty = ((CreateOrderActivity) getActivity()).getProductQtyOrdered(product);
+
+                updateQtyOnClick(position, productQty);
+            }
+        });
+
+        return rootView;
+    }
+
+    public void updateQtyOnClick(int position, int quantity) {
+        mGridData.get(position).setQty("x"+Integer.toString(quantity));
+        mGridAdapter.setGridData(mGridData);
+    }
+}
