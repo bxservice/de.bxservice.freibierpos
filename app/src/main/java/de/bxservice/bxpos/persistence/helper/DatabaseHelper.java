59eb30af21ab99ad8469326b06c3165ee99ed8ae
Diego Ruiz <12065321+d-ruiz@users.noreply.github.com>
2015-12-22 15:20:19 +0100

    Added tables sqsl clauses

--- app/src/main/java/de/bxservice/bxpos/persistence/helper/DatabaseHelper.java
+++ app/src/main/java/de/bxservice/bxpos/persistence/helper/DatabaseHelper.java
@@ -7,6 +7,7 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
 
+import de.bxservice.bxpos.logic.model.idempiere.Table;
 import de.bxservice.bxpos.logic.model.pos.PosUser;
 import de.bxservice.bxpos.persistence.dbcontract.GroupTableContract;
 import de.bxservice.bxpos.persistence.dbcontract.PosOrderContract;
@@ -329,6 +330,68 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         // updating row
         return db.update(Tables.TABLE_USER, values, UserColumns.USER_ID + " = ?",
                 new String[] { String.valueOf(user.getId()) });
+    }
+
+    /*
+    * Creating a table
+    */
+    public long createTable (Table table) {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put(TableContract.TableDB.COLUMN_NAME_TABLE_ID, table.getTableID());
+        values.put(TableContract.TableDB.COLUMN_NAME_GROUP_TABLE_ID, table.getBelongingGroup().getTableGroupID());
+        values.put(TableContract.TableDB.COLUMN_NAME_TABLE_NAME, table.getTableName());
+        values.put(TableContract.TableDB.COLUMN_NAME_TABLE_STATUS, table.getStatus());
+        values.put(TableContract.TableDB.COLUMN_NAME_VALUE, table.getValue());
+
+        // insert row
+        long tableId = db.insert(Tables.TABLE_TABLE, null, values);
+
+        return tableId;
+    }
+
+    /*
+    * get single table
+    */
+    public Table getTable(long table_id) {
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        String selectQuery = "SELECT  * FROM " + Tables.TABLE_TABLE + " WHERE "
+                + TableContract.TableDB.COLUMN_NAME_TABLE_ID + " = " + table_id;
+
+        Log.e(TAG, selectQuery);
+
+        Cursor c = db.rawQuery(selectQuery, null);
+
+        if (c != null)
+            c.moveToFirst();
+
+        Table table = new Table();
+        table.setTableID(c.getInt(c.getColumnIndex(TableContract.TableDB.COLUMN_NAME_TABLE_ID)));
+        table.setTableGroup(c.getInt(c.getColumnIndex(TableContract.TableDB.COLUMN_NAME_GROUP_TABLE_ID)));
+        table.setStatus((c.getString(c.getColumnIndex(TableContract.TableDB.COLUMN_NAME_TABLE_STATUS))));
+        table.setTableName(c.getString(c.getColumnIndex(TableContract.TableDB.COLUMN_NAME_TABLE_NAME)));
+        table.setValue((c.getString(c.getColumnIndex(TableContract.TableDB.COLUMN_NAME_VALUE))));
+
+        return table;
+    }
+
+    /*
+    * Updating a table
+    */
+    public int updateTable (Table table) {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put(TableContract.TableDB.COLUMN_NAME_GROUP_TABLE_ID, table.getBelongingGroup().getTableGroupID());
+        values.put(TableContract.TableDB.COLUMN_NAME_TABLE_NAME, table.getTableName());
+        values.put(TableContract.TableDB.COLUMN_NAME_TABLE_STATUS, table.getStatus());
+        values.put(TableContract.TableDB.COLUMN_NAME_VALUE, table.getValue());
+
+        // updating row
+        return db.update(Tables.TABLE_USER, values, UserColumns.USER_ID + " = ?",
+                new String[] { String.valueOf(table.getTableID()) });
     }
 
     // closing database
