fd4c7eec8b3c1937cc409dabf2c25870260b2fd4
Diego Ruiz <12065321+d-ruiz@users.noreply.github.com>
2015-12-30 10:13:39 +0100

    Get all tables groups with the corresponding tables

--- app/src/main/java/de/bxservice/bxpos/persistence/helper/PosTableGroupHelper.java
+++ app/src/main/java/de/bxservice/bxpos/persistence/helper/PosTableGroupHelper.java
@@ -6,6 +6,9 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.util.Log;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import de.bxservice.bxpos.logic.model.idempiere.TableGroup;
 import de.bxservice.bxpos.persistence.dbcontract.GroupTableContract;
 import de.bxservice.bxpos.persistence.definition.Tables;
@@ -77,4 +80,35 @@ public class PosTableGroupHelper extends PosObjectHelper {
         return db.update(Tables.TABLE_TABLE_GROUP, values, GroupTableContract.GroupTableDB.COLUMN_NAME_TABLE_GROUP_ID + " = ?",
                 new String[] { String.valueOf(tableGroup.getTableGroupID()) });
     }
+
+    /**
+     * Getting all orders
+     */
+    public List<TableGroup> getAllTableGroups() {
+        List<TableGroup> tableGroups = new ArrayList<>();
+        String selectQuery = "SELECT  * FROM " + Tables.TABLE_TABLE_GROUP;
+
+        Log.e(LOG_TAG, selectQuery);
+
+        SQLiteDatabase db = getReadableDatabase();
+        Cursor c = db.rawQuery(selectQuery, null);
+
+        // looping through all rows and adding to list
+        if (c.moveToFirst()) {
+            PosTableHelper tableHelper = new PosTableHelper(mContext);
+            do {
+                TableGroup tableGroup = new TableGroup();
+                tableGroup.setTableGroupID(c.getInt(c.getColumnIndex(GroupTableContract.GroupTableDB.COLUMN_NAME_TABLE_GROUP_ID)));
+                tableGroup.setValue((c.getString(c.getColumnIndex(GroupTableContract.GroupTableDB.COLUMN_NAME_VALUE))));
+                tableGroup.setName(c.getString(c.getColumnIndex(GroupTableContract.GroupTableDB.COLUMN_NAME_GROUP_TABLE_NAME)));
+                tableGroup.setTables(tableHelper.getAllTables(tableGroup));
+
+                // adding to orders list
+                tableGroups.add(tableGroup);
+            } while (c.moveToNext());
+        }
+
+        return tableGroups;
+    }
+
 }
