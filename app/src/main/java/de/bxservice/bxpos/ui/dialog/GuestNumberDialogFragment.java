dbcf00aacb8486b35a2d02e2d9f392ab02462a01
Diego Ruiz <12065321+d-ruiz@users.noreply.github.com>
2016-03-02 22:42:26 +0100

    Remove redundant modifiers

--- app/src/main/java/de/bxservice/bxpos/ui/dialog/GuestNumberDialogFragment.java
+++ app/src/main/java/de/bxservice/bxpos/ui/dialog/GuestNumberDialogFragment.java
@@ -21,7 +21,7 @@ public class GuestNumberDialogFragment extends DialogFragment {
      * implement this interface in order to receive event callbacks.
      * Each method passes the DialogFragment in case the host needs to query it. */
     public interface GuestNumberDialogListener {
-        public void onDialogPositiveClick(GuestNumberDialogFragment dialog);
+        void onDialogPositiveClick(GuestNumberDialogFragment dialog);
     }
 
     // Use this instance of the interface to deliver action events
