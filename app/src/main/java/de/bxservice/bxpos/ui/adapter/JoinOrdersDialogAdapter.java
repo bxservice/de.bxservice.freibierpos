2e23889f10574437aabd3daa2f81f325e24f8869
druiz <druiz@localhost>
2016-03-11 19:06:59 +0100

    Add all the UI components to join orders

---
+++ app/src/main/java/de/bxservice/bxpos/ui/adapter/JoinOrdersDialogAdapter.java
@@ -0,0 +1,89 @@
+package de.bxservice.bxpos.ui.adapter;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import de.bxservice.bxpos.R;
+import de.bxservice.bxpos.logic.model.idempiere.Table;
+import de.bxservice.bxpos.logic.model.pos.POSOrder;
+
+/**
+ * Created by Diego Ruiz on 11/03/16.
+ */
+public class JoinOrdersDialogAdapter extends RecyclerView.Adapter<JoinOrdersDialogAdapter.JoinOrdersDialogViewHolder>
+        implements View.OnClickListener {
+
+    private ArrayList<POSOrder> mDataset;
+    private View.OnClickListener listener;
+
+    // Provide a reference to the views for each data item
+    // Complex data items may need more than one view per item, and
+    // you provide access to all the views for a data item in a view holder
+    public static class JoinOrdersDialogViewHolder extends RecyclerView.ViewHolder {
+        // each data item is just a string in this case
+        public TextView txtOrder;
+        public TextView txtTable;
+
+        public JoinOrdersDialogViewHolder(View v) {
+            super(v);
+
+            txtOrder = (TextView) itemView.findViewById(R.id.order_title);
+            txtTable = (TextView) itemView.findViewById(R.id.table_name);
+        }
+
+        public void bindTable(POSOrder order) {
+            txtOrder.setText(String.valueOf(order.getOrderId()));
+            if (order.getTable() != null)
+                txtTable.setText(String.valueOf(order.getTable().getTableName()));
+            else
+                txtTable.setText(itemView.getResources().getString(R.string.unset_table));
+        }
+    }
+
+    // Provide a suitable constructor (depends on the kind of dataset)
+    public JoinOrdersDialogAdapter(ArrayList<POSOrder> myDataset) {
+        mDataset = myDataset;
+    }
+
+    // Create new views (invoked by the layout manager)
+    @Override
+    public JoinOrdersDialogViewHolder onCreateViewHolder(ViewGroup parent,
+                                                    int viewType) {
+        View v = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.join_orders_grid_item_layout, parent, false);
+        // set the view's size, margins, paddings and layout parameters
+
+        v.setOnClickListener(this);
+
+        return new JoinOrdersDialogViewHolder(v);
+    }
+
+    public void setOnClickListener(View.OnClickListener listener) {
+        this.listener = listener;
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (listener != null)
+            listener.onClick(view);
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    @Override
+    public void onBindViewHolder(JoinOrdersDialogViewHolder holder, int position) {
+        POSOrder order = mDataset.get(position);
+        holder.bindTable(order);
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    @Override
+    public int getItemCount() {
+        return mDataset.size();
+    }
+
+}
---
+++ app/src/main/java/de/bxservice/bxpos/ui/dialog/JoinOrdersDialogFragment.java
@@ -0,0 +1,116 @@
+package de.bxservice.bxpos.ui.dialog;
+
+import android.app.Activity;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+
+import java.util.ArrayList;
+
+import de.bxservice.bxpos.R;
+import de.bxservice.bxpos.logic.DataProvider;
+import de.bxservice.bxpos.logic.model.pos.POSOrder;
+import de.bxservice.bxpos.ui.RecyclerItemsListener;
+import de.bxservice.bxpos.ui.adapter.JoinOrdersDialogAdapter;
+
+/**
+ * Created by Diego Ruiz on 11/03/16.
+ */
+public class JoinOrdersDialogFragment extends DialogFragment {
+
+
+
+    public interface JoinOrdersDialogListener {
+        void onDialogPositiveClick(JoinOrdersDialogFragment dialog);
+    }
+
+    // Use this instance of the interface to deliver action events
+    JoinOrdersDialogListener mListener;
+    private ArrayList<POSOrder> mGridData;
+    private RecyclerView recyclerView;
+    private POSOrder order;
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        // Use the Builder class for convenient dialog construction
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+        // Inflate and set the layout for the dialog
+        // Pass null as the parent view because its going in the dialog layout
+        View view = inflater.inflate(R.layout.join_orders_dialog, null);
+
+        recyclerView = (RecyclerView) view.findViewById(R.id.open_order_list);
+
+        // use a grid layout manager with 2 columns
+        GridLayoutManager mLayoutManager = new GridLayoutManager(getActivity().getBaseContext(), 3);
+        recyclerView.setLayoutManager(mLayoutManager);
+
+        initGridData();
+
+        JoinOrdersDialogAdapter mGridAdapter = new JoinOrdersDialogAdapter(mGridData);
+
+        recyclerView.addOnItemTouchListener(
+                new RecyclerItemsListener(getActivity().getBaseContext(), recyclerView, new RecyclerItemsListener.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(View view, int position) {
+                        order = mGridData.get(position);
+                        mListener.onDialogPositiveClick(JoinOrdersDialogFragment.this);
+                        JoinOrdersDialogFragment.this.getDialog().dismiss();
+                    }
+
+                    @Override
+                    public void onItemLongClick(View view, int position) {
+                    }
+
+                })
+        );
+
+        recyclerView.setAdapter(mGridAdapter);
+
+        builder.setTitle(R.string.change_table);
+        builder.setView(view)
+                .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        JoinOrdersDialogFragment.this.getDialog().cancel();
+                    }
+                });
+
+        // Create the AlertDialog object and return it
+        return builder.create();
+    }
+
+    /**
+     * init the grid data with all the tables
+     */
+    private void initGridData() {
+        DataProvider dataProvider = new DataProvider(getActivity().getBaseContext());
+        mGridData = new ArrayList<>(dataProvider.getAllOpenOrders());
+    }
+
+    public POSOrder getOrder() {
+        return order;
+    }
+
+    // Override the Fragment.onAttach() method to instantiate the GuestNumberDialogListener
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        // Verify that the host activity implements the callback interface
+        try {
+            // Instantiate the NoticeDialogListener so we can send events to the host
+            mListener = (JoinOrdersDialogListener) activity;
+        } catch (ClassCastException e) {
+            // The activity doesn't implement the interface, throw exception
+            throw new ClassCastException(activity.toString()
+                    + " must implement JoinOrdersDialogListener");
+        }
+    }
+
+}
