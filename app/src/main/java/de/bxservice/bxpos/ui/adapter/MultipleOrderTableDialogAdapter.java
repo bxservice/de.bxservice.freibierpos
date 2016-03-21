6d386a84a496a08edf8124e51cd7c9854934af72
Diego Ruiz <12065321+d-ruiz@users.noreply.github.com>
2016-03-21 19:25:08 +0100

    Add way to select order when a table has multiple open orders

---
+++ app/src/main/java/de/bxservice/bxpos/ui/adapter/MultipleOrderTableDialogAdapter.java
@@ -0,0 +1,88 @@
+package de.bxservice.bxpos.ui.adapter;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import de.bxservice.bxpos.R;
+import de.bxservice.bxpos.logic.model.pos.POSOrder;
+
+/**
+ * Created by Diego Ruiz on 21/03/16.
+ */
+public class MultipleOrderTableDialogAdapter extends RecyclerView.Adapter<MultipleOrderTableDialogAdapter.MultipleOrderTableDialogViewHolder>
+        implements View.OnClickListener {
+
+    private ArrayList<POSOrder> mDataset;
+    private View.OnClickListener listener;
+
+    // Provide a reference to the views for each data item
+    // Complex data items may need more than one view per item, and
+    // you provide access to all the views for a data item in a view holder
+    public static class MultipleOrderTableDialogViewHolder extends RecyclerView.ViewHolder {
+        // each data item is just a string in this case
+        public TextView txtOrderId;
+        public TextView txtTotal;
+
+        public MultipleOrderTableDialogViewHolder(View v) {
+            super(v);
+
+            txtOrderId = (TextView) itemView.findViewById(R.id.order_id);
+            txtTotal = (TextView) itemView.findViewById(R.id.total_amount);
+        }
+
+        public void bindTable(POSOrder order) {
+            txtOrderId.setText(String.valueOf(order.getOrderId()));
+            if (order.getTable() != null)
+                txtTotal.setText(order.getTable().getTableName());
+            else
+                txtTotal.setText(itemView.getResources().getString(R.string.unset_table));
+        }
+    }
+
+    // Provide a suitable constructor (depends on the kind of dataset)
+    public MultipleOrderTableDialogAdapter(ArrayList<POSOrder> myDataset) {
+        mDataset = myDataset;
+    }
+
+    // Create new views (invoked by the layout manager)
+    @Override
+    public MultipleOrderTableDialogViewHolder onCreateViewHolder(ViewGroup parent,
+                                                         int viewType) {
+        View v = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.select_order_table_item_layout, parent, false);
+        // set the view's size, margins, paddings and layout parameters
+
+        v.setOnClickListener(this);
+
+        return new MultipleOrderTableDialogViewHolder(v);
+    }
+
+    public void setOnClickListener(View.OnClickListener listener) {
+        this.listener = listener;
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (listener != null)
+            listener.onClick(view);
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    @Override
+    public void onBindViewHolder(MultipleOrderTableDialogViewHolder holder, int position) {
+        POSOrder order = mDataset.get(position);
+        holder.bindTable(order);
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    @Override
+    public int getItemCount() {
+        return mDataset.size();
+    }
+
+}
---
+++ app/src/main/java/de/bxservice/bxpos/ui/dialog/MultipleOrdersTableDialogFragment.java
@@ -0,0 +1,113 @@
+package de.bxservice.bxpos.ui.dialog;
+
+import android.app.Activity;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import de.bxservice.bxpos.R;
+import de.bxservice.bxpos.logic.model.pos.POSOrder;
+import de.bxservice.bxpos.ui.RecyclerItemsListener;
+import de.bxservice.bxpos.ui.adapter.MultipleOrderTableDialogAdapter;
+import de.bxservice.bxpos.ui.decorator.DividerItemDecoration;
+
+/**
+ * Created by Diego Ruiz on 21/03/16.
+ */
+public class MultipleOrdersTableDialogFragment extends DialogFragment {
+
+
+
+    public interface MultipleOrdersTableDialogListener {
+        void onDialogPositiveClick(MultipleOrdersTableDialogFragment dialog);
+    }
+
+    // Use this instance of the interface to deliver action events
+    MultipleOrdersTableDialogListener mListener;
+    private ArrayList<POSOrder> mGridData;
+    POSOrder selectedOrder;
+    private RecyclerView recyclerView;
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        // Use the Builder class for convenient dialog construction
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+        // Inflate and set the layout for the dialog
+        // Pass null as the parent view because its going in the dialog layout
+        View view = inflater.inflate(R.layout.select_order_table_dialog, null);
+
+        recyclerView = (RecyclerView) view.findViewById(R.id.multiple_orders_list);
+
+        // use a grid layout manager with 2 columns
+        LinearLayoutManager mLayoutManager = new LinearLayoutManager(getActivity().getBaseContext());
+        mLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        recyclerView.setLayoutManager(mLayoutManager);
+        recyclerView.addItemDecoration(new DividerItemDecoration(getActivity().getBaseContext(), DividerItemDecoration.VERTICAL_LIST));
+
+        final MultipleOrderTableDialogAdapter mGridAdapter = new MultipleOrderTableDialogAdapter(mGridData);
+
+        recyclerView.addOnItemTouchListener(
+                new RecyclerItemsListener(getActivity().getBaseContext(), recyclerView, new RecyclerItemsListener.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(View view, int position) {
+                        selectedOrder = mGridData.get(position);
+                        mListener.onDialogPositiveClick(MultipleOrdersTableDialogFragment.this);
+                        MultipleOrdersTableDialogFragment.this.getDialog().dismiss();
+                    }
+
+                    @Override
+                    public void onItemLongClick(View view, int position) {
+                    }
+
+                })
+        );
+
+        recyclerView.setAdapter(mGridAdapter);
+
+        builder.setTitle(R.string.select_order);
+        builder.setView(view)
+                .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        MultipleOrdersTableDialogFragment.this.getDialog().cancel();
+                    }
+                });
+
+        // Create the AlertDialog object and return it
+        return builder.create();
+    }
+
+    public POSOrder getOrder() {
+        return selectedOrder;
+    }
+
+    public void setTableOrders(List<POSOrder> orders) {
+        mGridData = new ArrayList<>(orders);
+    }
+
+    // Override the Fragment.onAttach() method to instantiate the GuestNumberDialogListener
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        // Verify that the host activity implements the callback interface
+        try {
+            // Instantiate the NoticeDialogListener so we can send events to the host
+            mListener = (MultipleOrdersTableDialogListener) activity;
+        } catch (ClassCastException e) {
+            // The activity doesn't implement the interface, throw exception
+            throw new ClassCastException(activity.toString()
+                    + " must implement MultipleOrdersTableDialogListener");
+        }
+    }
+
+}
