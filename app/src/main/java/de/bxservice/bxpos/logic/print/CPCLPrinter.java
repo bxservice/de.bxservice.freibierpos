b4f3929e028e5cc3066a4988ac565c3699fc7da4
druiz <druiz@localhost>
2016-04-25 19:29:43 +0200

    Kitchen receipt printing properly

--- app/src/main/java/de/bxservice/bxpos/logic/print/CPCLPrinter.java
+++ app/src/main/java/de/bxservice/bxpos/logic/print/CPCLPrinter.java
@@ -1,6 +1,11 @@
 package de.bxservice.bxpos.logic.print;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 import de.bxservice.bxpos.logic.model.pos.POSOrder;
+import de.bxservice.bxpos.logic.model.pos.POSOrderLine;
 
 /**
  * Created by Diego Ruiz on 21/04/16.
@@ -15,71 +20,7 @@ public class CPCLPrinter extends AbstractPOSPrinter {
     @Override
     public String print() {
 
-        //Hello world example
-        /*String message = "! 0 200 200 210 1 \r\n" +
-                "TEXT 4 0 30 40 " + order.getStatus() +" \r\n" +
-                "FORM \r\n\r\n" +
-                "PRINT \r\n";/* +
-                "PRINT \r\n";
-
-        return message;*/
-
-        //Alignment example
-        /*message = "! 0 200 200 210 1 \r\n"+
-                "CENTER 383 \r\n"+
-                "TEXT 4 0 0 75 C \r\n"+
-                "LEFT \r\n"+
-                "CONCAT 4 0 \r\n" +
-                "4 2 5 Type: \r\n" +
-                "4 3 0 Dine \r\n" +
-                "4 2 5 in \r\n" +
-                "ENDCONCAT \r\n" +
-                //"TEXT 4 0 0 75 L \r\n"+
-                "RIGHT 383 \r\n" +
-                "TEXT 4 0 0 75 R \r\n"+
-                "FORM \r\n\r\n"+
-                "PRINT\r\n";*/
-
-        /*return "! 0 200 200 210 1 \r\n" +
-                "MULTILINE 47 \r\n" +
-                "TEXT 4 0 10 20 \r\n" +
-                "1st line of text \r\n" +
-                "2nd line of text \r\n" +
-                "Nth line of text \r\n" +
-                "ENDMULTILINE \r\n" +
-                "FORM \r\n\r\n"+
-                "PRINT \r\n";*/
-
-        /* Zebra example for a parking ticket
-        return String.format("! 0 200 200 930 1\r\n" +
-                "PW 384\r\n" +
-                "TONE 0\r\n" +
-                "SPEED 2\r\n" +
-                "ON-FEED IGNORE\r\n" +
-                "NO-PACE\r\n" +
-                "POSTFEED 152\r\n" +
-                "JOURNAL\r\n" +
-                "BOX 0 0 376 797 5\r\n" +
-                "T 5 1 85 27 Traffic Ticket Demo\r\n" +
-                "T 5 1 75 89 %s\r\n" +
-                "T 5 0 35 522 Priors:\r\n" +
-                "T 5 0 35 421 Address:\r\n" +
-                "T 5 0 35 342 Last Name:\r\n" +
-                "T 5 0 35 257 First Name:\r\n" +
-                "T 5 0 35 175 Plate #:\r\n" +
-                "T 5 0 64 210 %s\r\n" +
-                "T 5 0 64 290 %s\r\n" +
-                "T 5 0 64 374 %s\r\n" +
-                "T 5 0 64 466 %s\r\n" +
-                "T 5 0 64 558 %s\r\n" +
-                "B PDF-417 54 639 XD 3 YD 4 C 1 S 0\r\n" +
-                "Zebra Technologies makes printing from Smart Phones easy!\r\n" +
-                "ENDPDF\r\n" +
-                "BOX 11 789 376 920 8\r\n\r\n " +
-                "FORM \r\n\r\n"+
-                "PRINT\r\n", new Object[] { "Thsi", "KRUIS", "Carlos", "Ruiz", "AAA 2356", "None" });*/
-
-        return String.format("! 0 200 200 600 1\r\n" +
+        /*return String.format("! 0 200 200 600 1\r\n" +
                 "PW 550\r\n" +
                 "TONE 0\r\n" +
                 "SPEED 2\r\n" +
@@ -108,7 +49,46 @@ public class CPCLPrinter extends AbstractPOSPrinter {
                 "FORM \r\n\r\n"+
                 "PRINT\r\n", new Object[] { "Dine-in", "Garden", "5", "Table 1","Carlos", "Ruiz", "AAA 2356", "None" });
 
-        //return m;
+                /*return "! U1 JOURNAL\r\n" +
+                "! U1 SETLP 5 2 24\r\n" +
+                "Order #: "+ order.getOrderId() +"\r\n" +
+                "Type: Dine-in\r\n" +
+                "Server: Garden\r\n" +
+                "Guests: "+ order.getGuestNumber() +"\r\n" +
+                //"! U1 LINE 25 175 530 175 1\r\n" +
+                "! U1 SETLP 5 0 24\r\n" +
+
+                "! U1 SETLP 7 0 24\r\n" +
+                order.getOrderedLines().get(0).getQtyOrdered() + "  " + order.getOrderedLines().get(0).getProduct().getProductName() + "\r\n" +
+                order.getOrderedLines().get(1).getQtyOrdered() + "  " + order.getOrderedLines().get(1).getProduct().getProductName() + "\r\n" +
+                "! U1 PRESENT-AT\r\n" +
+                "! U1 PRINT\r\n";*/
+
+        DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+        Calendar cal = Calendar.getInstance();
+
+        StringBuilder ticket = new StringBuilder();
+
+        ticket.append("! U1 JOURNAL\r\n");
+        ticket.append("! U1 SETLP 5 3 86\r\n");
+        ticket.append("\r\n");
+        ticket.append("Order #: "+ order.getOrderId() +"\r\n");
+        ticket.append( "! U1 SETLP 7 0 24\r\n");
+        ticket.append( "Type: Dine-in\r\n");
+        ticket.append("Server: Garden\r\n");
+        ticket.append( "Guests: "+ order.getGuestNumber() +"\r\n");
+        ticket.append("\r\n");
+        ticket.append( "! U1 SETLP 5 2 46\r\n");
+        for(POSOrderLine line : order.getOrderedLines())
+            ticket.append(line.getQtyOrdered() + "  " + line.getProduct().getProductName() + "\r\n");
+        ticket.append("\r\n");
+        ticket.append( "! U1 SETLP 7 0 24\r\n");
+        ticket.append(dateFormat.format(cal.getTime())+"\r\n");//2014/08/06 16:00:22
+        ticket.append("! U1 PRESENT-AT\r\n");
+        ticket.append("! U1 PRINT\r\n");
+
+        return ticket.toString();
+
     }
 
     public void sendCommand(String paramString) {
