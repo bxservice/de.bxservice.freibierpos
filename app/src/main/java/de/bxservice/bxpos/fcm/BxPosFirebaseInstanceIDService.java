d8c936c77391a1620b0d5c8e656dfac61d1dde14
Diego Ruiz <12065321+d-ruiz@users.noreply.github.com>
2016-05-20 18:51:59 +0200

    add support to receive fcm notifications from iDempiere

---
+++ app/src/main/java/de/bxservice/bxpos/fcm/BxPosFirebaseInstanceIDService.java
@@ -0,0 +1,44 @@
+package de.bxservice.bxpos.fcm;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+/**
+ * Created by Diego Ruiz on 5/20/16.
+ */
+public class BxPosFirebaseInstanceIDService extends FirebaseInstanceIdService {
+
+    private static final String TAG = "BxPosFirebaseIIDService";
+
+    /**
+     * Called if InstanceID token is updated. This may occur if the security of
+     * the previous token had been compromised. Note that this is called when the InstanceID token
+     * is initially generated so this is where you would retrieve the token.
+     */
+    @Override
+    public void onTokenRefresh() {
+        // Get updated InstanceID token.
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d(TAG, "Refreshed token: " + refreshedToken);
+
+        System.out.println("MAYONNNEEEESA 2" );
+
+        // TODO: Implement this method to send registration via web services to iDempiere
+        sendRegistrationToServer(refreshedToken);
+    }
+
+    /**
+     * Persist token to third-party servers.
+     *
+     * Modify this method to associate the user's FCM InstanceID token with any server-side account
+     * maintained by your application.
+     *
+     * @param token The new token.
+     */
+    private void sendRegistrationToServer(String token) {
+        // Add custom implementation, as needed.
+        System.out.println("New token created ----> " + token);
+    }
+}
