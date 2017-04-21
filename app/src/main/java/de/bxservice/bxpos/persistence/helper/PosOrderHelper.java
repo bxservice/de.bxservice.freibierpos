6f84c8a8bb08cff066e5aa08a6866dd6c4d14a3e
Diego Ruiz <12065321+d-ruiz@users.noreply.github.com>
2017-04-21 18:16:40 +0200

    POSOrderHelper code refactor

--- app/src/main/java/de/bxservice/bxpos/persistence/helper/PosOrderHelper.java
+++ app/src/main/java/de/bxservice/bxpos/persistence/helper/PosOrderHelper.java
@@ -83,7 +83,7 @@ public class PosOrderHelper extends PosObjectHelper {
     /*
     * get single order
     */
-    public POSOrder getOrder (long order_id) {
+    public POSOrder getOrder(long order_id) {
         SQLiteDatabase db = getReadableDatabase();
 
         String selectQuery = "SELECT  * FROM " + Tables.TABLE_POSORDER + " WHERE "
@@ -117,7 +117,7 @@ public class PosOrderHelper extends PosObjectHelper {
     /*
     * Updating an order
     */
-    public int updateOrder (POSOrder order) {
+    public int updateOrder(POSOrder order) {
         SQLiteDatabase db = getWritableDatabase();
 
         ContentValues values = new ContentValues();
@@ -147,43 +147,6 @@ public class PosOrderHelper extends PosObjectHelper {
                 new String[] { String.valueOf(order.getOrderId()) });
     }
 
-    /**
-     * Getting all orders
-     */
-    public List<POSOrder> getAllOrders() {
-        List<POSOrder> orders = new ArrayList<>();
-        String selectQuery = "SELECT  * FROM " + Tables.TABLE_POSORDER;
-
-        Log.d(LOG_TAG, selectQuery);
-
-        SQLiteDatabase db = getReadableDatabase();
-        Cursor c = db.rawQuery(selectQuery, null);
-
-        // looping through all rows and adding to list
-        if (c.moveToFirst()) {
-            PosOrderLineHelper orderLineHelper = new PosOrderLineHelper(mContext);
-            do {
-                POSOrder order = new POSOrder();
-                order.setOrderId(c.getInt(c.getColumnIndex(PosOrderContract.POSOrderDB.COLUMN_NAME_ORDER_ID)));
-                order.setStatus(c.getString(c.getColumnIndex(PosOrderContract.POSOrderDB.COLUMN_NAME_ORDER_STATUS)));
-                order.setGuestNumber(c.getInt(c.getColumnIndex(PosOrderContract.POSOrderDB.COLUMN_NAME_GUESTS)));
-                order.setOrderRemark(c.getString(c.getColumnIndex(PosOrderContract.POSOrderDB.COLUMN_NAME_REMARK)));
-                order.setTotalFromInt(c.getInt(c.getColumnIndex(PosOrderContract.POSOrderDB.COLUMN_NAME_TOTALLINES)));
-                order.setTable(c.getInt(c.getColumnIndex(PosOrderContract.POSOrderDB.COLUMN_NAME_TABLE_ID)));
-                order.setDocumentNo(c.getString(c.getColumnIndex(PosOrderContract.POSOrderDB.COLUMN_NAME_DOCUMENT_NO)));
-                order.setOrderingLines(orderLineHelper.getAllOrderingLines(order));
-                order.setOrderedLines(orderLineHelper.getAllOrderedLines(order));
-
-                // adding to orders list
-                orders.add(order);
-            } while (c.moveToNext());
-
-            c.close();
-        }
-
-        return orders;
-    }
-
     public int deleteOrder(POSOrder order) {
         SQLiteDatabase db = getWritableDatabase();
 
@@ -193,7 +156,7 @@ public class PosOrderHelper extends PosObjectHelper {
     /*
     * get single order by table
     */
-    public POSOrder getOrder (Table table) {
+    public POSOrder getOrder(Table table) {
         SQLiteDatabase db = getReadableDatabase();
 
         String selectQuery = "SELECT  * FROM " + Tables.TABLE_POSORDER + " WHERE "
@@ -232,97 +195,20 @@ public class PosOrderHelper extends PosObjectHelper {
     /*
     * get all orders by table
     */
-    public ArrayList<POSOrder> getTableOrders (Table table) {
-        ArrayList<POSOrder> orders = new ArrayList<>();
-
-        SQLiteDatabase db = getReadableDatabase();
-
-        String selectQuery = "SELECT  * FROM " + Tables.TABLE_POSORDER + " WHERE "
-                + PosOrderContract.POSOrderDB.COLUMN_NAME_TABLE_ID + " = ? AND "
+    public ArrayList<POSOrder> getTableOrders(Table table) {
+        String whereClause = " WHERE " + PosOrderContract.POSOrderDB.COLUMN_NAME_TABLE_ID + " = ? AND "
                 + PosOrderContract.POSOrderDB.COLUMN_NAME_ORDER_STATUS + " = ? ";
 
-        Log.d(LOG_TAG, selectQuery);
-
-        Cursor c = db.rawQuery(selectQuery, new String[] {String.valueOf(table.getTableID()), POSOrder.SENT_STATUS});
-
-        if (c.moveToFirst()) {
-            PosOrderLineHelper orderLineHelper = new PosOrderLineHelper(mContext);
-            do {
-                POSOrder order = new POSOrder();
-                order.setOrderId(c.getInt(c.getColumnIndex(PosOrderContract.POSOrderDB.COLUMN_NAME_ORDER_ID)));
-                order.setStatus(c.getString(c.getColumnIndex(PosOrderContract.POSOrderDB.COLUMN_NAME_ORDER_STATUS)));
-                order.setGuestNumber(c.getInt(c.getColumnIndex(PosOrderContract.POSOrderDB.COLUMN_NAME_GUESTS)));
-                order.setOrderRemark(c.getString(c.getColumnIndex(PosOrderContract.POSOrderDB.COLUMN_NAME_REMARK)));
-                order.setTotalFromInt(c.getInt(c.getColumnIndex(PosOrderContract.POSOrderDB.COLUMN_NAME_TOTALLINES)));
-                order.setPaymentRule(c.getString(c.getColumnIndex(PosOrderContract.POSOrderDB.COLUMN_NAME_PAYMENT_RULE)));
-                order.setDocumentNo(c.getString(c.getColumnIndex(PosOrderContract.POSOrderDB.COLUMN_NAME_DOCUMENT_NO)));
-
-                order.setTable(table);
-                order.setOrderingLines(orderLineHelper.getAllOrderingLines(order));
-                order.setOrderedLines(orderLineHelper.getAllOrderedLines(order));
-
-                Boolean flag = (c.getInt(c.getColumnIndex(PosOrderContract.POSOrderDB.COLUMN_NAME_SYNCHRONIZED)) != 0);
-                order.setSync(flag);
-
-                order.setCurrentLineNo();
-
-                // adding to orders list
-                orders.add(order);
-            } while (c.moveToNext());
-
-            c.close();
-        }
-
-        return orders;
-    }
+        return getOrders(whereClause, new String[] {String.valueOf(table.getTableID()), POSOrder.SENT_STATUS});    }
 
     /**
      * Get all open orders
      * @return
      */
     public ArrayList<POSOrder> getOpenOrders() {
-        ArrayList<POSOrder> orders = new ArrayList<>();
+        String whereClause =" WHERE " + PosOrderContract.POSOrderDB.COLUMN_NAME_ORDER_STATUS + " NOT IN (?,?)";
 
-        String selectQuery = "SELECT  * FROM " + Tables.TABLE_POSORDER +
-                " WHERE " + PosOrderContract.POSOrderDB.COLUMN_NAME_ORDER_STATUS + " NOT IN (?,?)";
-
-
-        Log.d(LOG_TAG, selectQuery);
-
-        SQLiteDatabase db = this.getReadableDatabase();
-        Cursor c = db.rawQuery(selectQuery, new String[] {POSOrder.COMPLETE_STATUS, POSOrder.VOID_STATUS});
-
-        // looping through all rows and adding to list
-        if (c.moveToFirst()) {
-            PosOrderLineHelper orderLineHelper = new PosOrderLineHelper(mContext);
-            do {
-                POSOrder order = new POSOrder();
-                order.setOrderId(c.getInt(c.getColumnIndex(PosOrderContract.POSOrderDB.COLUMN_NAME_ORDER_ID)));
-                order.setStatus(c.getString(c.getColumnIndex(PosOrderContract.POSOrderDB.COLUMN_NAME_ORDER_STATUS)));
-                order.setGuestNumber(c.getInt(c.getColumnIndex(PosOrderContract.POSOrderDB.COLUMN_NAME_GUESTS)));
-                order.setOrderRemark(c.getString(c.getColumnIndex(PosOrderContract.POSOrderDB.COLUMN_NAME_REMARK)));
-                order.setTotalFromInt(c.getInt(c.getColumnIndex(PosOrderContract.POSOrderDB.COLUMN_NAME_TOTALLINES)));
-                if(c.getColumnIndex(PosOrderContract.POSOrderDB.COLUMN_NAME_TABLE_ID) != -1 &&
-                        c.getInt(c.getColumnIndex(PosOrderContract.POSOrderDB.COLUMN_NAME_TABLE_ID)) != 0)
-                    order.setTable(c.getInt(c.getColumnIndex(PosOrderContract.POSOrderDB.COLUMN_NAME_TABLE_ID)));
-                order.setOrderingLines(orderLineHelper.getAllOrderingLines(order));
-                order.setOrderedLines(orderLineHelper.getAllOrderedLines(order));
-                order.setPaymentRule(c.getString(c.getColumnIndex(PosOrderContract.POSOrderDB.COLUMN_NAME_PAYMENT_RULE)));
-                order.setDocumentNo(c.getString(c.getColumnIndex(PosOrderContract.POSOrderDB.COLUMN_NAME_DOCUMENT_NO)));
-
-                Boolean flag = (c.getInt(c.getColumnIndex(PosOrderContract.POSOrderDB.COLUMN_NAME_SYNCHRONIZED)) != 0);
-                order.setSync(flag);
-
-                order.setCurrentLineNo();
-
-                // adding to orders list
-                orders.add(order);
-            } while (c.moveToNext());
-
-            c.close();
-        }
-
-        return orders;
+        return getOrders(whereClause, new String[] {POSOrder.COMPLETE_STATUS, POSOrder.VOID_STATUS});
     }
 
     /**
@@ -330,17 +216,29 @@ public class PosOrderHelper extends PosObjectHelper {
      * @return
      */
     public ArrayList<POSOrder> getUnsynchronizedOrders() {
-        ArrayList<POSOrder> orders = new ArrayList<>();
-
-        String selectQuery = "SELECT  * FROM " + Tables.TABLE_POSORDER +
-                " WHERE " + PosOrderContract.POSOrderDB.COLUMN_NAME_ORDER_STATUS + " IN (?,?) " +
+        String whereClause = " WHERE " + PosOrderContract.POSOrderDB.COLUMN_NAME_ORDER_STATUS + " IN (?,?) " +
                 " AND " + PosOrderContract.POSOrderDB.COLUMN_NAME_SYNCHRONIZED + " = ?" ;
 
+        return getOrders(whereClause, new String[] {POSOrder.COMPLETE_STATUS, POSOrder.VOID_STATUS, Integer.toString(0)});
+    }
 
-        Log.d(LOG_TAG, selectQuery);
+    /**
+     * Get all orders
+     * @return an array list of orders that satisfy the where clause
+     */
+    private ArrayList<POSOrder> getOrders(String whereClause, String[] args) {
+        ArrayList<POSOrder> orders = new ArrayList<>();
+
+        StringBuilder selectQuery = new StringBuilder();
+
+        selectQuery.append("SELECT  * FROM ");
+        selectQuery.append(Tables.TABLE_POSORDER);
+        selectQuery.append(whereClause);
+
+        Log.d(LOG_TAG, selectQuery.toString());
 
         SQLiteDatabase db = this.getReadableDatabase();
-        Cursor c = db.rawQuery(selectQuery, new String[] {POSOrder.COMPLETE_STATUS, POSOrder.VOID_STATUS, Integer.toString(0)});
+        Cursor c = db.rawQuery(selectQuery.toString(), args);
 
         // looping through all rows and adding to list
         if (c.moveToFirst()) {
@@ -355,28 +253,31 @@ public class PosOrderHelper extends PosObjectHelper {
                 order.setSurchargeFromInt(c.getInt(c.getColumnIndex(PosOrderContract.POSOrderDB.COLUMN_NAME_SURCHARGE)));
                 order.setDiscountFromInt(c.getInt(c.getColumnIndex(PosOrderContract.POSOrderDB.COLUMN_NAME_DISCOUNT)));
                 order.setDiscountReason(c.getString(c.getColumnIndex(PosOrderContract.POSOrderDB.COLUMN_NAME_DISCOUNT_REASON)));
+                order.setPaymentRule(c.getString(c.getColumnIndex(PosOrderContract.POSOrderDB.COLUMN_NAME_PAYMENT_RULE)));
+                order.setDocumentNo(c.getString(c.getColumnIndex(PosOrderContract.POSOrderDB.COLUMN_NAME_DOCUMENT_NO)));
                 if(c.getColumnIndex(PosOrderContract.POSOrderDB.COLUMN_NAME_TABLE_ID) != -1 &&
                         c.getInt(c.getColumnIndex(PosOrderContract.POSOrderDB.COLUMN_NAME_TABLE_ID)) != 0)
                     order.setTable(c.getInt(c.getColumnIndex(PosOrderContract.POSOrderDB.COLUMN_NAME_TABLE_ID)));
                 order.setOrderingLines(orderLineHelper.getAllOrderingLines(order));
                 order.setOrderedLines(orderLineHelper.getAllOrderedLines(order));
-                order.setPaymentRule(c.getString(c.getColumnIndex(PosOrderContract.POSOrderDB.COLUMN_NAME_PAYMENT_RULE)));
-                order.setDocumentNo(c.getString(c.getColumnIndex(PosOrderContract.POSOrderDB.COLUMN_NAME_DOCUMENT_NO)));
 
                 Boolean flag = (c.getInt(c.getColumnIndex(PosOrderContract.POSOrderDB.COLUMN_NAME_SYNCHRONIZED)) != 0);
                 order.setSync(flag);
 
-                if(IOrder.PAYMENTRULE_MixedPOSPayment.equals(order.getPaymentRule())) {
+                if (IOrder.PAYMENTRULE_MixedPOSPayment.equals(order.getPaymentRule())) {
                     PosPaymentHelper paymentHelper = new PosPaymentHelper(mContext);
                     order.setPayments(paymentHelper.getAllPayments(order));
                 }
 
+                order.setCurrentLineNo();
+
                 // adding to orders list
                 orders.add(order);
             } while (c.moveToNext());
-
-            c.close();
         }
+
+        if (c != null)
+            c.close();
 
         return orders;
     }
@@ -440,51 +341,11 @@ public class PosOrderHelper extends PosObjectHelper {
      * @return array list of paid orders in a time frame
      */
     public ArrayList<POSOrder> getPaidOrders(long fromDate, long toDate) {
-        ArrayList<POSOrder> orders = new ArrayList<>();
 
-        String selectQuery = "SELECT  * FROM " + Tables.TABLE_POSORDER +
-                " WHERE " + PosOrderContract.POSOrderDB.COLUMN_NAME_ORDER_STATUS + " IN (?,?) " +
+        String whereClause =" WHERE " + PosOrderContract.POSOrderDB.COLUMN_NAME_ORDER_STATUS + " IN (?,?) " +
                 " AND " + PosOrderContract.POSOrderDB.COLUMN_NAME_UPDATED_AT + " BETWEEN ? AND ? ";
 
-        Log.d(LOG_TAG, selectQuery);
-
-        SQLiteDatabase db = this.getReadableDatabase();
-        Cursor c = db.rawQuery(selectQuery, new String[] {POSOrder.COMPLETE_STATUS, POSOrder.VOID_STATUS, String.valueOf(fromDate), String.valueOf(toDate)});
-
-        // looping through all rows and adding to list
-        if (c.moveToFirst()) {
-            PosOrderLineHelper orderLineHelper = new PosOrderLineHelper(mContext);
-            do {
-                POSOrder order = new POSOrder();
-                order.setOrderId(c.getInt(c.getColumnIndex(PosOrderContract.POSOrderDB.COLUMN_NAME_ORDER_ID)));
-                order.setStatus(c.getString(c.getColumnIndex(PosOrderContract.POSOrderDB.COLUMN_NAME_ORDER_STATUS)));
-                order.setGuestNumber(c.getInt(c.getColumnIndex(PosOrderContract.POSOrderDB.COLUMN_NAME_GUESTS)));
-                order.setOrderRemark(c.getString(c.getColumnIndex(PosOrderContract.POSOrderDB.COLUMN_NAME_REMARK)));
-                order.setTotalFromInt(c.getInt(c.getColumnIndex(PosOrderContract.POSOrderDB.COLUMN_NAME_TOTALLINES)));
-                order.setSurchargeFromInt(c.getInt(c.getColumnIndex(PosOrderContract.POSOrderDB.COLUMN_NAME_SURCHARGE)));
-                order.setDiscountFromInt(c.getInt(c.getColumnIndex(PosOrderContract.POSOrderDB.COLUMN_NAME_DISCOUNT)));
-                order.setDiscountReason(c.getString(c.getColumnIndex(PosOrderContract.POSOrderDB.COLUMN_NAME_DISCOUNT_REASON)));
-                order.setPaymentRule(c.getString(c.getColumnIndex(PosOrderContract.POSOrderDB.COLUMN_NAME_PAYMENT_RULE)));
-                order.setDocumentNo(c.getString(c.getColumnIndex(PosOrderContract.POSOrderDB.COLUMN_NAME_DOCUMENT_NO)));
-                if(c.getColumnIndex(PosOrderContract.POSOrderDB.COLUMN_NAME_TABLE_ID) != -1 &&
-                        c.getInt(c.getColumnIndex(PosOrderContract.POSOrderDB.COLUMN_NAME_TABLE_ID)) != 0)
-                    order.setTable(c.getInt(c.getColumnIndex(PosOrderContract.POSOrderDB.COLUMN_NAME_TABLE_ID)));
-                order.setOrderingLines(orderLineHelper.getAllOrderingLines(order));
-                order.setOrderedLines(orderLineHelper.getAllOrderedLines(order));
-
-                Boolean flag = (c.getInt(c.getColumnIndex(PosOrderContract.POSOrderDB.COLUMN_NAME_SYNCHRONIZED)) != 0);
-                order.setSync(flag);
-
-                order.setCurrentLineNo();
-
-                // adding to orders list
-                orders.add(order);
-            } while (c.moveToNext());
-
-            c.close();
-        }
-
-        return orders;
+        return getOrders(whereClause, new String[] {POSOrder.COMPLETE_STATUS, POSOrder.VOID_STATUS, String.valueOf(fromDate), String.valueOf(toDate)});
     }
 
     /**
@@ -494,54 +355,12 @@ public class PosOrderHelper extends PosObjectHelper {
      * @return array list of paid orders in a time frame
      */
     public ArrayList<POSOrder> getUserPaidOrders(long fromDate, long toDate) {
-        ArrayList<POSOrder> orders = new ArrayList<>();
 
-        int userId = getLoggedUser();
-
-        String selectQuery = "SELECT  * FROM " + Tables.TABLE_POSORDER +
-                " WHERE " + PosOrderContract.POSOrderDB.COLUMN_NAME_ORDER_STATUS + " IN (?,?) " +
+        String whereClause = " WHERE " + PosOrderContract.POSOrderDB.COLUMN_NAME_ORDER_STATUS + " IN (?,?) " +
                 " AND " + PosOrderContract.POSOrderDB.COLUMN_NAME_CREATED_BY + " = ? " +
                 " AND " + PosOrderContract.POSOrderDB.COLUMN_NAME_UPDATED_AT + " BETWEEN ? AND ? ";
 
-        Log.d(LOG_TAG, selectQuery);
-
-        SQLiteDatabase db = this.getReadableDatabase();
-        Cursor c = db.rawQuery(selectQuery, new String[] {POSOrder.COMPLETE_STATUS, POSOrder.VOID_STATUS, String.valueOf(userId), String.valueOf(fromDate), String.valueOf(toDate)});
-
-        // looping through all rows and adding to list
-        if (c.moveToFirst()) {
-            PosOrderLineHelper orderLineHelper = new PosOrderLineHelper(mContext);
-            do {
-                POSOrder order = new POSOrder();
-                order.setOrderId(c.getInt(c.getColumnIndex(PosOrderContract.POSOrderDB.COLUMN_NAME_ORDER_ID)));
-                order.setStatus(c.getString(c.getColumnIndex(PosOrderContract.POSOrderDB.COLUMN_NAME_ORDER_STATUS)));
-                order.setGuestNumber(c.getInt(c.getColumnIndex(PosOrderContract.POSOrderDB.COLUMN_NAME_GUESTS)));
-                order.setOrderRemark(c.getString(c.getColumnIndex(PosOrderContract.POSOrderDB.COLUMN_NAME_REMARK)));
-                order.setTotalFromInt(c.getInt(c.getColumnIndex(PosOrderContract.POSOrderDB.COLUMN_NAME_TOTALLINES)));
-                order.setSurchargeFromInt(c.getInt(c.getColumnIndex(PosOrderContract.POSOrderDB.COLUMN_NAME_SURCHARGE)));
-                order.setDiscountFromInt(c.getInt(c.getColumnIndex(PosOrderContract.POSOrderDB.COLUMN_NAME_DISCOUNT)));
-                order.setDiscountReason(c.getString(c.getColumnIndex(PosOrderContract.POSOrderDB.COLUMN_NAME_DISCOUNT_REASON)));
-                order.setPaymentRule(c.getString(c.getColumnIndex(PosOrderContract.POSOrderDB.COLUMN_NAME_PAYMENT_RULE)));
-                order.setDocumentNo(c.getString(c.getColumnIndex(PosOrderContract.POSOrderDB.COLUMN_NAME_DOCUMENT_NO)));
-                if(c.getColumnIndex(PosOrderContract.POSOrderDB.COLUMN_NAME_TABLE_ID) != -1 &&
-                        c.getInt(c.getColumnIndex(PosOrderContract.POSOrderDB.COLUMN_NAME_TABLE_ID)) != 0)
-                    order.setTable(c.getInt(c.getColumnIndex(PosOrderContract.POSOrderDB.COLUMN_NAME_TABLE_ID)));
-                order.setOrderingLines(orderLineHelper.getAllOrderingLines(order));
-                order.setOrderedLines(orderLineHelper.getAllOrderedLines(order));
-
-                Boolean flag = (c.getInt(c.getColumnIndex(PosOrderContract.POSOrderDB.COLUMN_NAME_SYNCHRONIZED)) != 0);
-                order.setSync(flag);
-
-                order.setCurrentLineNo();
-
-                // adding to orders list
-                orders.add(order);
-            } while (c.moveToNext());
-
-            c.close();
-        }
-
-        return orders;
+        return getOrders(whereClause, new String[] {POSOrder.COMPLETE_STATUS, POSOrder.VOID_STATUS, String.valueOf(getLoggedUser()), String.valueOf(fromDate), String.valueOf(toDate)});
     }
 
     public int getMaxDocumentNo(String orderPrefix) {
